ffcfcd28bfe14547a75e79253a0313fcf95fb2b2
Jared Hancock <gravydish@gmail.com>
2018-02-04 20:22:45 -0600

    oops: Fix memory corruption in garbage collection

--- src/Objects/garbage.h
+++ src/Objects/garbage.h
@@ -6,8 +6,8 @@
 void
 garbage_collect(Object* object);
 
-#define INCREF(object) ((Object*) object)->refcount++
-#define DECREF(object) do { if (0 == --((Object*) object)->refcount) garbage_collect(object); } while(0)
+#define INCREF(object) ((Object*) (object))->refcount++
+#define DECREF(object) do { if (0 == --(((Object*) (object))->refcount)) garbage_collect((Object*) (object)); } while(0)
 // TODO: Trigger garbage collection if refcount==0
 
 #endif
