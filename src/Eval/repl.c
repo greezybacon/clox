greezybacon/clox

commit 422d432119174769076c9313a5b1e223b50351b6

repl: Fix multiline interpretation

src/Eval/repl.c
@@ -91,10 +91,11 @@ repl_loop(CmdLoop* self) {
             continue;
         }
 
-        stop = self->onecmd(self, line);
+        stop = self->onecmd(self, buffer);
         if (self->postcmd)
             stop = self->postcmd(self, stop, line);
 
+        pbuffer = buffer;
         prompt = self->prompt;
     }
     free(buffer);

commit b5773481471bd6cd61b5ce8bed72b7048f27fe43

repl: Fix infinite repeat of last statement on CTRL+D
Regression from 422d432

src/Eval/repl.c
@@ -82,13 +82,15 @@ repl_loop(CmdLoop* self) {
             printf("%s", prompt);
 
         line = fgets(pbuffer, (2048 - (pbuffer - buffer)) * sizeof(char), stdin);
-        if (!line)
-            line = "EOF";
-
-        pbuffer += strlen(line);
-        if (eval_repl_isdangling(self, buffer, pbuffer - buffer)) {
-            prompt = self->prompt2;
-            continue;
+        if (!line) {
+            strcpy(buffer, "EOF");
+        }
+        else {
+            pbuffer += strlen(line);
+            if (eval_repl_isdangling(self, buffer, pbuffer - buffer)) {
+                prompt = self->prompt2;
+                continue;
+            }
         }
 
         stop = self->onecmd(self, buffer);
@@ -96,6 +98,7 @@ repl_loop(CmdLoop* self) {
             stop = self->postcmd(self, stop, line);
 
         pbuffer = buffer;
+        buffer[0] = '\0';
         prompt = self->prompt;
     }
     free(buffer);