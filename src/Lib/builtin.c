165a50b8f4a0b961a9a589cf521faa86ab067638
Jared Hancock <gravydish@gmail.com>
2018-10-10 21:40:05 -0500

    builtin: Add additional built-in function concepts

--- src/Lib/builtin.c
+++ src/Lib/builtin.c
@@ -40,12 +40,63 @@ builtin_int(VmScope* state, Object* self, Object* args) {
     return LoxNIL;
 }
 
+static Object*
+builtin_len(VmScope* state, Object* self, Object* args) {
+    assert(Tuple_isTuple(args));
+
+    size_t argc = Tuple_getSize(args);
+    Object* arg = Tuple_getItem((TupleObject*) args, 0);
+
+    if (arg->type->len) {
+        return arg->type->len(arg);
+    }
+
+    // TODO: Raise error
+    return LoxNIL;
+}
+
+static Object*
+builtin_eval(VmScope *state, Object *self, Object *args) {
+    assert(Tuple_isTuple(args));
+
+    size_t argc = Tuple_getSize(args);
+    if (argc != 1)
+        printf("eval() takes exactly one argument\n");
+
+    Object* arg = Tuple_getItem((TupleObject*) args, 0);
+    StringObject *string;
+    if (!String_isString(arg)) {
+        if (arg->type->as_string)
+            string = (StringObject*) arg->type->as_string(arg);
+        else
+            // Error
+            printf("eval: cannot coerce argument to string\n");
+    }
+    else {
+        string = (StringObject*) arg;
+    }
+
+    VmScope scope = (VmScope) {
+        .globals = state->globals,
+    };
+    return vmeval_string_inscope(string->characters, string->length, &scope);
+}
+
+static Object*
+builtin_tuple(VmScope *state, Object *self, Object *args) {
+    assert(Tuple_isTuple(args));
+    return args;
+}
+
 static ModuleDescription
 builtins_module_def = {
     .name = "__builtins__",
     .methods = {
         { "print", builtin_print },
         { "int", builtin_int },
+        { "len", builtin_len },
+        { "eval", builtin_eval },
+        { "tuple", builtin_tuple },
         { 0 },
     }
 };
