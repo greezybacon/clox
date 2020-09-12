91b3b762e590ca4916d2d53c7a26470dcae2cda1
Jared Hancock <gravydish@gmail.com>
2020-09-11 20:47:21 -0500

    range: Add a special optimization for integers
    The range object is generic and can handle floats or integers. So this
    adds a special case for the integer to keep the addition in C for better
    performance.

--- src/Objects/range.c
+++ src/Objects/range.c
@@ -2,11 +2,33 @@
 
 #include "Compile/vm.h"
 
+#include "integer.h"
 #include "iterator.h"
 #include "object.h"
 #include "range.h"
 
 static struct object_type LoxRangeType;
+static struct object_type LoxIntRangeType;
+
+Object*
+LoxRange_create(Object *start, Object *stop, Object *step) {
+    if (Integer_isInteger(start) && Integer_isInteger(stop) && Integer_isInteger(step)) {
+        LoxIntRange *range = object_new(sizeof(LoxIntRange), &LoxIntRangeType);
+        range->start = Integer_toInt(start);
+        range->end = Integer_toInt(stop);
+        range->step = Integer_toInt(step);
+        return (Object*) range;
+    }
+
+    LoxRange *range = object_new(sizeof(LoxRange), &LoxRangeType);
+    range->start = start;
+    range->end = stop;
+    range->step = step;
+
+    return (Object*) range;
+}
+
+// A generic range object which also supports floats
 
 static Object*
 loxrange_next(Iterator *self) {
@@ -22,16 +44,6 @@ loxrange_next(Iterator *self) {
     return LoxStopIteration;
 }
 
-Object*
-LoxRange_create(Object *start, Object *stop, Object *step) {
-    LoxRange *range = object_new(sizeof(LoxRange), &LoxRangeType);
-    range->start = start;
-    range->end = stop;
-    range->step = step;
-
-    return (Object*) range;
-}
-
 Iterator*
 range_iterate(Object *self) {
     assert(self->type == &LoxRangeType);
@@ -45,6 +57,39 @@ range_iterate(Object *self) {
 static struct object_type LoxRangeType = (ObjectType) {
     .name = "range",
     .iterate = range_iterate,
+
+    //.as_string = range_asstring,
+};
+
+// A specific range object optimized for integer operations
+
+static Object*
+loxrange_next_int(Iterator *self) {
+    LoxIntRange *range = (LoxIntRange*) self->target;
+    LoxIntRangeIterator *iter = (LoxIntRangeIterator*) self;
+
+    long long *current = &iter->current;
+    *current = *current + range->step;
+
+    if (*current < range->end)
+        return (Object*) Integer_fromLongLong(*current);
+
+    return LoxStopIteration;
+}
+
+Iterator*
+range_iterate_int(Object *self) {
+    assert(self->type == &LoxIntRangeType);
+
+    LoxIntRangeIterator* iter = (LoxIntRangeIterator*) LoxIterator_create(self, sizeof(LoxRangeIterator));
+    iter->iterator.next = loxrange_next_int;
+    iter->current = ((LoxIntRange*) self) ->start;
+    return (Iterator*) iter;
+}
+
+static struct object_type LoxIntRangeType = (ObjectType) {
+    .name = "range",
+    .iterate = range_iterate_int,
 
     //.as_string = range_asstring,
 };
--- src/Objects/range.h
+++ src/Objects/range.h
@@ -21,6 +21,23 @@ typedef struct {
     Object      *current;
 } LoxRangeIterator;
 
+typedef struct {
+    union {
+        Object      object;
+    };
+    long long   start;
+    long long   end;
+    long long   step;
+} LoxIntRange;
+
+typedef struct {
+    union {
+        Object      object;
+        Iterator    iterator;
+    };
+    long long   current;
+} LoxIntRangeIterator;
+
 Object* LoxRange_create(Object*, Object*, Object*);
 
 #endif
